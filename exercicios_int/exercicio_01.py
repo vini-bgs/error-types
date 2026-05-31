d6abbb60aec862b63fab904a582868821b87b97b
vini-bgs <viniciustheros@gmail.com>
2026-05-31 10:45:06 -0300

    resolução do exercício sobre a variável 'int'.

---
+++ exercicios_int/exercicio_01.py
@@ -0,0 +1,33 @@
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+num1 = int(input("Digite um número inteiro: "))
+
+num2 = int(input("Digite outro número inteiro: "))
+
+soma = num1 + num2
+
+print(f"A soma {num1} + {num2} = {soma}.")
+
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+resto = num1 % 5
+
+print(f"O resto da divisão de {num1} com 5 é {resto}.")
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+multi = num1 * num2
+
+print(f"O resultado da multiplicação entre {num1} e {num2} é {multi}")
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+div_int = num1 // num2
+
+print(f"A divisão inteira de {num1} e {num2} é {div_int}.")
+
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+quadrado = num2 ** 2
+
+print(f"O quadrado de {num2} é igual a {quadrado}")
